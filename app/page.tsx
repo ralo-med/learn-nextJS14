e00494771051358cb73cf468469e9ff70c410bdb
Ralo <74175604+ralo-med@users.noreply.github.com>
2025-07-10 10:45:54 +0900

    #2.2 Not Found Routes

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Navigation from "../components/navigation";
+
+export default function Home() {
+  return (
+    <div>
+      <Navigation />
+      <h1>Hello Next.js</h1>
+    </div>
+  );
+}
