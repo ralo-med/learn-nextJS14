92221cc0a638968fe2ea8272b698e4512958a843
Ralo <74175604+ralo-med@users.noreply.github.com>
2025-07-10 11:08:39 +0900

    #SSR vs CSR next는 일단 서버 렌더링임

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,14 +1,20 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
+  const pathname = usePathname();
   return (
     <nav>
       <ul>
         <li>
           <Link href="/">Home</Link>
+          {pathname === "/" ? "✅" : ""}
         </li>
         <li>
           <Link href="/about-us">About Us</Link>
+          {pathname === "/about-us" ? "✅" : ""}
         </li>
       </ul>
     </nav>
