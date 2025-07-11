e7108f4ea438d5f83ea283e7a5e438a34cc12ee2
Ralo <74175604+ralo-med@users.noreply.github.com>
2025-07-11 11:57:40 +0900

    #Clients Side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,21 +1,43 @@
+"use client";
+
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  interface Movie {
+    id: number;
+    title: string;
+    rating: number;
+  }
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovies(json);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <div>
       <h1>Hello Next.js</h1>
       <div>
         <h2>Movies</h2>
         <ul>
-          <li>
-            <Link href="/movies/1">Movie 1</Link>
-          </li>
-          <li>
-            <Link href="/movies/2">Movie 2</Link>
-          </li>
-          <li>
-            <Link href="/movies/3">Movie 3</Link>
-          </li>
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            movies.map((movie) => (
+              <li key={movie.id}>
+                <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+              </li>
+            ))
+          )}
         </ul>
       </div>
     </div>
